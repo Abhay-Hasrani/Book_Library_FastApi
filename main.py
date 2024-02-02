d87b731c00814e339a0b5bd1161934036323e0e7
Abhay Hasrani <abhayhasrani@gmail.com>
2024-02-02 14:25:44 +0530

    add alembic for easy database migrations

--- main.py
+++ main.py
@@ -13,31 +13,3 @@ load_dotenv()
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-@app.get("/")
-async def get_users():
-    db = SessionLocal()
-    users = db.query(User).all()
-    books = db.query(Book).all()
-    userbooks = db.query(UserBook).all()
-    return [users,books,userbooks]
-
-@app.get("/user")
-async def get_users(): 
-    db = SessionLocal()
-    # new_user = User(email='example@example.com', username='example_user', hashed_password='hashed_password', role='user')
-    # db.add(new_user)
-    new_book = Book(
-    imageUrl="http://example.com/book.jpg",
-    title="Sample Book",
-    description="This is a sample book description.",
-    author="Sample Author",
-    launched=date(2022, 1, 1),
-    rating=5
-    )
-    db.add(new_book)
-    db.commit()
-    return {}
